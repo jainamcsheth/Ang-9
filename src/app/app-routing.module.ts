5ffcefdaa7596aae8aa08ea9f56d088619db8bc7
Jainam Sheth <jainamcsheth@gmail.com>
2020-05-25 15:37:00 +0530

    Fixing assets route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
     path: 'posts',
     component: PostsComponent,
   },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
 ];
 
 @NgModule({
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'personal-blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-blog');
+    s;
   });
 
   it('should render title', () => {
